18065819a80eb7e07fa8a143a81fed53e5552ca8
Milo <milohatten@gmail.com>
2025-04-04 07:56:51 -0500

    added more updates to lever puzzle

--- Assets/Scripts/lever puzzle/LeverPull.cs
+++ Assets/Scripts/lever puzzle/LeverPull.cs
@@ -1,35 +1,25 @@
-// using System.Collections;
-// using System.Collections.Generic;
-// using UnityEngine;
-// using UnityEngine.UI;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
 
-// public class LeverPull : MonoBehaviour{
-//     public Levers lever;
-//     public puzzle_main puzzle;
-//     private Image img;
-//     private float initAlpha;
-//     // Start is called before the first frame update
-//     void Start(){
-//         img = GetComponent<Image>();
-//         initAlpha = img.lever.a;
-//     }
+public class LeverPull : MonoBehaviour{
+    public Levers lever;
+    public puzzle_main puzzle;
 
-//     // Update is called once per frame
-//     void Update(){
-//         if (img.lever.a > initAlpha){
-//             Levers l = img.lever;
-//             l.a -= 1.0f * Time.deltaTime;
-//             img.lever = l;
-//         }
-//     }
+    // Start is called before the first frame update
+    void Start(){
 
-//     public void Switch(){
-//         if (puzzle.State != LeverPuzzleState._4_NORTH_FINISHED){
-//             print ("KRONK PULLED THIS LEVER: " + lever);
-//             Levers l = img.lever;
-//             l.a = 1.0f; 
-//             img.lever = l;
-//             puzzle.Switch(lever);
-//         }
-//     }
-// }
+    }
+
+    // Update is called once per frame
+    void Update(){
+    }
+
+    public void Switch(){
+        if (puzzle.State != LeverPuzzleState._4_NORTH_FINISHED){
+            print ("KRONK PULLED THIS LEVER: " + lever);
+            puzzle.Switch(lever);
+        }
+    }
+}
