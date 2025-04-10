02b1b7cb3a2b281921bcc3ff71149c00bf3bfa62
Cheoddox <leeroywycoff@gmail.com>
2025-04-10 13:49:22 -0500

    Updated my puzzle visually and improved DoorOneOpen script
    Updated my puzzle to have a top, a glass pane in front, and a small wall to somewhat hide the battery disappearing when it enters the deposit boxes, as well as two pipes out of the top of each of the walls (mainly to hide the object im using for the battery),
    Updated the DoorOneOpen script to instead use the door close animation so the door opens enough to be walked through

--- Assets/Scripts/DoorOneOpen.cs
+++ Assets/Scripts/DoorOneOpen.cs
@@ -22,6 +22,6 @@ public class DoorOneOpen : MonoBehaviour
     }
 
     public void OpenDoor() {
-        animator.Play("Base Layer.Door Open");
+        animator.Play("Base Layer.Door Close");
     }
 }
